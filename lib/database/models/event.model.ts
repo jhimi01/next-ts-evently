00dd28a470b4a0d999dd7caadf137a0d97255fb5
jhimi01 <talukderjhimi@gmail.com>
2024-05-06 17:04:55 +0600

    problem solved

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,4 +1,4 @@
-import { models, model, Schema, Types } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
@@ -9,29 +9,28 @@ export interface IEvent extends Document {
   imageUrl: string;
   startDateTime: Date;
   endDateTime: Date;
-  price?: string;
+  price: string;
   isFree: boolean;
   url?: string;
-  catergory?: { _id: string; name: string };
-  organizer?: { _id: string; firstName: string; lastName: string };
+  category: { _id: string, name: string }
+  organizer: { _id: string, firstName: string, lastName: string }
 }
 
 const EventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   location: { type: String },
-  date: { type: Date, required: true },
-  createAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
   imageUrl: { type: String, required: true },
   startDateTime: { type: Date, default: Date.now },
   endDateTime: { type: Date, default: Date.now },
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
-  organizer: { type: Schema.Types.ObjectId, ref: "User" },
-});
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+})
 
-const Event = models.Event || model("Event", EventSchema);
+const Event = models.Event || model('Event', EventSchema);
 
 export default Event;
